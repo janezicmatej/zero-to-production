0f4fed9737c0d1116600a51ab61382c2518c2286
Matej Janežič <janezic.mj@gmail.com>
2023-06-03 00:56:12 +0200

    chapter: 3.6..=3.7

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
@@ -5,14 +7,28 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 // const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscriptions(form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscriptions))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -27,3 +27,49 @@ async fn test_health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
+
+#[tokio::test]
+async fn subscribe_valid_cases() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20doge&email=muchwow%40gmail.com";
+
+    let response = client
+        .post(&format!("{address}/subscriptions"))
+        .body(body)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    assert!(response.status().is_success());
+}
+
+#[tokio::test]
+async fn subscribe_error_cases() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let tests = [
+        ("name=ledoge", "missing email"),
+        ("email=ledoge@gmail.com", "missing name"),
+        ("", "missing email and name"),
+    ];
+
+    for (body, reason) in tests {
+        let response = client
+            .post(format!("{address}/subscriptions"))
+            .body(body)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .send()
+            .await
+            .expect("failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "didn't fail with 400 when there was {reason}",
+        );
+    }
+}
